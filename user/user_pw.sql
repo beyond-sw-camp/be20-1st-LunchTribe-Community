1702ad590945feb69cb7f535806c27a9eb467653
sangdo19909-code <sangdo19909@gmail.com>
2025-08-30 23:52:11 +0900

    #21 사용자 비밀번호를 재설정
    사용자 아이디와 이름을 조회한 후 비밀번호를 찾고 재설정 한다.

--- user/user_pw.sql
+++ user/user_pw.sql
@@ -9,13 +9,16 @@
 --    SET user_pw = 'javayanolja'(재설정 비밀번호)
 --  WHERE user_id = 'korsh111'
 --    AND user_pw = 'dltkdeh90'(기존 비밀번호)
+--    AND USER_name = '이상도'(아아디만으론 해킹위험이 있어 이름을 추가함)
 
 
 SELECT user_pw
   FROM member
-WHERE user_id = 'korsh111';
+WHERE user_id = 'korsh111'
+ 
 
 UPDATE member
    SET user_pw = 'javayanolja'
  WHERE user_id = 'korsh111'
    AND user_pw = 'dltkdeh90'
+   AND USER_name = '이상도'
