26c0d3dcd45582245e6b74082185371b9bf8d0a5
waifuser <madobe7@naver.com>
2025-09-01 15:10:31 +0900

    더미 99개 넣는 반복문

--- member/signup.sql
+++ member/signup.sql
@@ -24,3 +24,42 @@ VALUES
   , 'M'
   , 25
 )
+
+
+-- 더미 100개 만드는 반복문
+DELIMITER //
+CREATE PROCEDURE dowhile()
+BEGIN
+  DECLARE i INT DEFAULT 1;
+
+  WHILE i < 100 DO
+    INSERT
+		INTO member
+		(
+		    `student_code`
+		  , `role_code` 
+		  , `user_id` 
+		  , `user_name` 
+		  , `user_pw` 
+		  , `user_phone` 
+		  , `nick_name` 
+		  , `gender` 
+		  , `age` 
+		)
+		VALUES
+		(
+			 i
+		  , 1
+		  , CONCAT('a', CONCAT(i))
+		  , CONCAT('이름', CONCAT(i))
+  		  , CONCAT('pw', CONCAT(i))
+  		  , CONCAT(i)
+  		  , CONCAT('닉네임', CONCAT(i))
+  		  , 'M'
+  		  , 25
+		);
+    SET i = i + 1;
+  END WHILE;
+END //
+DELIMITER ;
+CALL dowhile();
