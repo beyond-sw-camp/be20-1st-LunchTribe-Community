11003f44838732051ad3acb6e16eae915d0a8cbd
sangdo19909-code <sangdo19909@gmail.com>
2025-09-01 20:45:04 +0900

    #26 회원 정보 조회 수정 기능
    회원정보 조회 수정 기능 업데이트 했습니다.

--- member/member_edit.sql
+++ member/member_edit.sql
@@ -2,42 +2,42 @@
 
 -- 회원 코드user_code(pk)로 회원코드, 역할, 아이디, 비밀번호 등을 변경한다.
 UPDATE member
-   SET student_code = 0999
+   SET student_code = 0999								AS '학생번호'
  WHERE user_code = 1;
   
 UPDATE member
-   SET role_code = 3
+   SET role_code = 3										AS '역할코드'
  WHERE user_code = 1; 
  
 UPDATE member
-   SET user_id = 'okokok123'
+   SET user_id = 'okokok123'							AS '아이디'
  WHERE user_code = 1;
  
  
 UPDATE member
-   SET user_name = '강성현'
+   SET user_name = '강성현'							AS '이름'
  WHERE user_code = 1;
  
  
 UPDATE member
-   SET user_pw = 'okok321'
+   SET user_pw = 'okok321'								AS '비밀번호'
  WHERE user_code = 1;
  
  
 UPDATE member
-   SET user_phone = '010-0012-0034'
+   SET user_phone = '010-0012-0034'					AS '전화번호'
  WHERE user_code = 1;
  
  
 UPDATE member
-   SET nick_name = 'baby'
+   SET nick_name = 'baby'								AS '닉네임'
  WHERE user_code = 1;
  
 UPDATE member
-   SET gender = '여'
+   SET gender = '여'									  AS  '성별'
  WHERE user_code = 1;
  
  
 UPDATE member
-   SET age = 77
+   SET age = 77											AS '나이'
  WHERE user_code = 1;
--- member/member_search.sql
+++ member/member_search.sql
@@ -5,11 +5,35 @@
 --   FROM MEMBER;
 -- WHERE user_code = (INT)'자신의 유저 코드(pk)'
 
-SELECT *
+SELECT 
+		 user_code						AS '유저코드'
+	  , student_code					AS '학생번호'
+	  , role_code						AS '역할코드'
+	  , user_name						AS '이름'
+	  , user_id							AS '아이디'
+	  , user_pw							AS '비밀번호'
+	  , user_phone						AS '전화번호'
+	  , nick_name						AS '닉네임'
+	  , gender							AS '성별'
+	  , age								AS '나이'
+	  , user_created_at				AS '가입날짜'
+	  , user_updated_at				AS '수정날짜'
   FROM member
- WHERE user_code = 1;
+ WHERE user_code = 1;			/*로그인한 사람의 유저코드(pk)를 받아와 자동입력*/
  
 
  -- 관리자가 회원 전체를 조회하는 기능입니다.
-SELECT *
-  FROM MEMBER;
+SELECT 
+      user_code						AS '유저코드'
+	  , student_code					AS '학생번호'
+	  , role_code						AS '역할코드'
+	  , user_name					   AS '이름'
+	  , user_id							AS '아이디'
+	  , user_pw							AS '비밀번호'
+	  , user_phone						AS '전화번호'
+	  , nick_name						AS '닉네임'
+	  , gender							AS '성별'
+	  , age								AS '나이'
+	  , user_created_at				AS '가입날짜'
+	  , user_updated_at				AS '수정날짜'
+  FROM member;
