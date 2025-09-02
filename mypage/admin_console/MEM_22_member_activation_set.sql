3ba7f63e3c1d6a53600357ce45059a2b99ef2979
waifuser <madobe7@naver.com>
2025-09-02 17:16:08 +0900

    매일 자정 이벤트 스케줄러 작성 완료
    훈련 종료 기간 다음날에 계정 비활성화

--- mypage/admin_console/MEM_22_member_activation_set.sql
+++ mypage/admin_console/MEM_22_member_activation_set.sql
@@ -6,6 +6,36 @@ UPDATE member
 	SET STATUS ='N' 		/*변경할 상태를 Y,N 중 선택*/
  WHERE user_code = 1;	/*변경할 사용자의 고유식별자를 입력*/	
 
+-- 작동중인 이벤트 스케줄러와 이벤트 스케줄러 설정 확인
+SHOW EVENTS;
+
+SHOW VARIABLES LIKE 'event_scheduler';
+
+-- root로 실행
+SET GLOBAL event_scheduler = ON;
+
+-- 매 자정에 전날이 훈련 종료일일경우 계정 비활성화(status값을 N으로 변경)
+CREATE EVENT IF NOT EXISTS update_sign_in_disable
+ON SCHEDULE EVERY 1 DAY
+STARTS NOW()																	-- 테스트용 지금 즉시 실행
+-- STARTS TIMESTAMP(CURDATE() + INTERVAL 1 DAY)						-- 내일 자정부터 실행
+ON COMPLETION PRESERVE
+DO
+  UPDATE member a
+    JOIN student b ON a.student_code = b.student_code
+     SET a.status = 'N'														-- 상태를 N으로 변경
+   WHERE b.training_end = SUBDATE(CURDATE(), INTERVAL 1 DAY);	-- 어제가 훈련 종료일이면
+
+-- 실행중인 이벤트 스케줄러들 재확인
+SHOW EVENTS;
+
+-- 이벤트 스케줄러 update_sign_in_disable 삭제
+DROP EVENT update_sign_in_disable;
+
+-- update_sign_in_disable 스케줄러 재실행
+-- ALTER EVENT update_sign_in_disable ENABLE;
+
+
 
 /*
 DROP TRIGGER IF EXISTS member_update;
